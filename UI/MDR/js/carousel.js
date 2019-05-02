9fcff0704d5e3f0ec2f8d786492dfd4a6ffbef06
william-parker <edwinious@gmail.com>
2019-05-02 12:21:15 -0400

    finished next and prev arrows on carousel

--- UI/MDR/js/carousel.js
+++ UI/MDR/js/carousel.js
@@ -10,3 +10,23 @@ const slideWidth = slides[0].getBoundingClientRect().width;
 const setSlidePosition = (slide, index) => {slide.style.left = slideWidth * index + 'px';};
 
 slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(track, currentSlide, prevSlide);
+})
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+
+    moveToSlide(track, currentSlide, nextSlide);
+})
