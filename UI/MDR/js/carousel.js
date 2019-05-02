cd4b007d8b61b624309e6203877663ec668abd49
william-parker <edwinious@gmail.com>
2019-05-02 11:33:40 -0400

    EP Initial Commit

---
+++ UI/MDR/js/carousel.js
@@ -0,0 +1,12 @@
+const track = document.querySelector('.carousel_track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel_button--right');
+const prevButton = document.querySelector('.carousel_button--left');
+const dotsNav = document.querySelector('.carousel_nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const setSlidePosition = (slide, index) => {slide.style.left = slideWidth * index + 'px';};
+
+slides.forEach(setSlidePosition);
